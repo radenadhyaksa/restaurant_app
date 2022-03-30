b505ed8520522994eaa9b82dbb4d3e172d9b47f9
radenadhyaksa <raksa69@gmail.com>
2022-03-30 14:48:27 +0700

    update res_detail.dart

--- lib/restaurant_detail.dart
+++ lib/restaurant_detail.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/model/model_restaurant.dart';
 
 var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
 
