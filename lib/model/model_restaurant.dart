e81ba5649839a68253612a6a51cab937134dce7f
radenadhyaksa <raksa69@gmail.com>
2022-03-30 14:43:09 +0700

    update model_res.dart

---
+++ lib/model/model_restaurant.dart
@@ -0,0 +1,74 @@
+class PresidenIndonesia {
+  String name;
+  String period1;
+  String description;
+  String years;
+  String imageAsset;
+
+  PresidenIndonesia({
+    required this.name,
+    required this.period1,
+    required this.description,
+    required this.years,
+    required this.imageAsset,
+  });
+}
+
+var presidenList = [
+  PresidenIndonesia(
+    name: 'Soekarno',
+    period1: 'Presiden ke-1',
+    description:
+        'Dr. (H.C.) Ir. H. Soekarno1 (ER, EYD: Sukarno, nama lahir: Koesno Sosrodihardjo; 6 Juni 1901 – 21 Juni 1970)adalah Presiden pertama Republik Indonesia yang menjabat pada periode 1945–1967.',
+    years: '1945 - 1967',
+    imageAsset: 'images/soekarno1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'Soeharto',
+    period1: 'Presiden ke-2',
+    description:
+        'Jenderal Besar TNI (Purn.) H. M. Soeharto, (ER, EYD: Suharto; 8 Juni 1921 – 27 Januari 2008) adalah Presiden kedua Indonesia yang menjabat dari tahun 1967 sampai 1998, menggantikan Soekarno.',
+    years: '1968 - 1998',
+    imageAsset: 'images/soeharto-1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'B. J. Habibie ',
+    period1: 'Presiden ke-3',
+    description:
+        'Prof. Dr.ing. Ir. H. Bacharuddin Jusuf Habibie, FREng (25 Juni 1936 – 11 September 2019) adalah Presiden Republik Indonesia yang ketiga. Sebelumnya, B.J. Habibie menjabat sebagai Wakil Presiden Republik Indonesia ke-7, menggantikan Try Sutrisno.',
+    years: '1998 - 1999',
+    imageAsset: 'images/habibie-1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'Abdurahman Wahid',
+    period1: 'Presiden ke-4',
+    description:
+        'Dr. (H.C.). K.H. Abdurrahman Wahid (lahir dengan nama Abdurrahman ad-Dakhil; 7 September 1940 – 30 Desember 2009), atau yang akrab disapa Gus Dur, adalah tokoh Muslim Indonesia dan pemimpin politik yang menjadi Presiden Indonesia yang keempat dari tahun 1999 hingga 2001.',
+    years: '1999 - 2001',
+    imageAsset: 'images/gusdur-1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'Megawati Soekarnoputri',
+    period1: 'Presiden ke-5',
+    description:
+        'Prof. Dr. (H.C.) Hj. Diah Permata Megawati Setiawati Soekarnoputri (lahir 23 Januari 1947) adalah Presiden Indonesia yang kelima yang menjabat sejak 23 Juli 2001 sampai 20 Oktober 2004. Ia merupakan presiden wanita Indonesia pertama dan putri dari presiden Indonesia pertama, Soekarno.',
+    years: '2001 - 2004',
+    imageAsset: 'images/megawati-1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'Susilo Bambang Yudhoyono',
+    period1: 'Presiden ke-6',
+    description:
+        'Jenderal TNI (HOR) (Purn.) Prof. Dr. Dr. (HC). H. Susilo Bambang Yudhoyono, M.A. (lahir 9 September 1949) adalah Presiden Indonesia keenam yang menjabat sejak 20 Oktober 2004 hingga 20 Oktober 2014. Ia adalah Presiden pertama di Indonesia yang dipilih melalui jalur pemilihan umum.',
+    years: '2004 - 2014',
+    imageAsset: 'images/sby-1.jpg',
+  ),
+  PresidenIndonesia(
+    name: 'Joko Widodo',
+    period1: 'Presiden ke-7',
+    description:
+        'Ir. H. Joko Widodo. lahir 21 Juni 1961, atau yang lebih akrab disapa Jokowi, adalah Presiden Indonesia yang mulai menjabat sejak 20 Oktober 2014. Terpilih dalam Pemilu Presiden 2014, Jokowi menjadi Presiden Indonesia pertama sepanjang sejarah yang bukan berasal dari latar belakang elite politik atau militer Indonesia.',
+    years: '2014 - saat ini',
+    imageAsset: 'images/jokowi-1.jpg',
+  ),
+];
