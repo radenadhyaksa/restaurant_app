c9a4d7ae71381a8e2617d4d69e917a3128f18430
radenadhyaksa <raksa69@gmail.com>
2022-03-30 14:31:19 +0700

    update restaurant_screen.dart

---
+++ lib/restaurant_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Presiden Indonesia'),
+      ),
+
+      //navigation bar
+      // body: _pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person_rounded),
+          ),
+          BottomNavigationBarItem(
+            label: 'Berita',
+            icon: Icon(Icons.newspaper),
+          ),
+        ],
+        // onTap: (index) => setState(() => currentIndex = index),
+      ),
+
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final PresidenIndonesia place = presidenList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place: place);
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(place.imageAsset),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(place.period1),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: presidenList.length,
+      ),
+    );
+  }
+}
