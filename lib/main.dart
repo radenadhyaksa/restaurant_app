d6f776fcd00f09ecec4e4ed56f06a1c8b153259c
radenadhyaksa <raksa69@gmail.com>
2022-03-30 15:01:50 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Presiden Indonesia',
+      title: 'Restaurant LokalPunya',
       theme: ThemeData(),
       home: RestaurantScreen(),
     );
