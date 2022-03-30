fc5b52ee90f4f4437e6fbf9072507b29f77c7f1c
radenadhyaksa <raksa69@gmail.com>
2022-03-30 16:56:04 +0700

    update price

--- lib/model/model_restaurant.dart
+++ lib/model/model_restaurant.dart
@@ -3,6 +3,7 @@ class ModelRestaurant {
   String khas;
   String description;
   String rating;
+  String price;
   String imageAsset;
 
   ModelRestaurant({
@@ -10,6 +11,7 @@ class ModelRestaurant {
     required this.khas,
     required this.description,
     required this.rating,
+    required this.price,
     required this.imageAsset,
   });
 }
@@ -21,6 +23,7 @@ var menuList = [
     description:
         'Nasi uduk merupakan hidangan gurih dari nasi kelapa khas dari Jakarta. Nasi uduk adalah olahan dari nasi kukus yang umumnya dicampur dengan rempah-rempah. Hidangan ini dibuat dari beras yang dicampur santan, cengkeh, kayu manis, dan serai. /n Sajian ini punya cita rasa yang memanjakan lidah dengan topping bawang goreng. Lauk pelengkapnya seperti tempe orek, telur, dan krecek.',
     rating: '4.5',
+    price: 'Rp 20.000',
     imageAsset: 'images/nasi-uduk.jpg',
   ),
   ModelRestaurant(
@@ -29,6 +32,7 @@ var menuList = [
     description:
         'Nasi goreng adalah makanan nusantara sederhana yang super lezat. Selain bumbu rempah, nasi goreng biasanya ditambah beberapa bahan lain supaya lebih nikmat, seperti daging, sayuran, dan telur goreng. Bahan rahasia yang membuat nasi goreng makin nikmat adalah kecap manis yang membuat warna nasi seperti karamel. Nasi goreng juga disajikan bersama irisan tomat segar, selada, kerupuk, dan mentimun.',
     rating: '4.8',
+    price: 'Rp 35.000',
     imageAsset: 'images/nasi-goreng.jpg',
   ),
   ModelRestaurant(
@@ -37,6 +41,7 @@ var menuList = [
     description:
         'Gudeg merupakan makanan tradisional Jogja dan Jawa Tengah. Gudeg dibuat dari nangka mentah yang direbus selama berjam-jam dengan campuran santan, bumbu rempah, dan gula aren. Gudeg biasa disajikan bersama telur, ayam, dan nasi putih. Gudeg memiliki cita rasa sedikit manis dan pekat.',
     rating: '4.7',
+    price: 'Rp 20.000',
     imageAsset: 'images/nasi-gudeg.jpg',
   ),
   ModelRestaurant(
@@ -45,6 +50,7 @@ var menuList = [
     description:
         'Gado-gado adalah makanan khas Jakarta berisi sayur-sayuran yang direbus, irisan telur dan tahu, serta ditaburi bawang goreng dan kerupuk. Sayur-sayuran ditambahkan dengan bumbu kacang atau saus dari kacang tanah yang telah dihaluskan yang kemudian diaduk merata.',
     rating: '4.4',
+    price: 'Rp 20.000',
     imageAsset: 'images/gado-gadoo.jpg',
   ),
   ModelRestaurant(
@@ -53,6 +59,7 @@ var menuList = [
     description:
         'Sumatera Barat terkenal dengan makanan Padang yang berasal dari kota Padang. Makanan yang banyak rempahnya ini mempunyai rasa yang kuat. Rendang adalah salah satu masakan Padang yang menjadi favorit banyak orang Indonesiabahkan sampai luar negeri.',
     rating: '5.0',
+    price: 'Rp 35.000',
     imageAsset: 'images/rendang.jpg',
   ),
   ModelRestaurant(
@@ -61,6 +68,7 @@ var menuList = [
     description:
         'Di Sumatera Selatan terkenal makanan Pempek. Pempek terbuat dari ikan dan sagu. Penyajianya ditemani dengan kuah cokelat yang disebut cuko. Cuko terbuat dari air yang dididihkan, kemudian ditambah gula merah, udang rebi dan cabe rawit tumbuk, bawang putih, dan garam.',
     rating: '4.7',
+    price: 'Rp 28.000',
     imageAsset: 'images/pempek.jpg',
   ),
   ModelRestaurant(
@@ -69,6 +77,7 @@ var menuList = [
     description:
         'Soto Banjar adalah sosto khas suku Banjar, Kalimantan Selatan dengan bahan utama ayam dan beraroma harum rempah-rempah seperti kayu manis, biji pala, dan cengkeh. Soto berisi daging ayam yang sudah disuwir-suwir dengan tambahan perkedel atau kentang rebus, rebusan telur, dan ketupat.',
     rating: '4.6',
+    price: 'Rp 25.000',
     imageAsset: 'images/soto-banjar.jpg',
   ),
 ];
